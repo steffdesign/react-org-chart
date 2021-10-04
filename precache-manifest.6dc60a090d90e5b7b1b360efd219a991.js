79890d1ce212eac8d92bf6845939285ebad1cd08
Steffany <steffany@nalarocks.com>
2021-10-04 16:18:46 -0300

    Updates

---
+++ precache-manifest.6dc60a090d90e5b7b1b360efd219a991.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fa67e632d6c6f961dc128b199bd420",
+    "url": "/react-org-chart/index.html"
+  },
+  {
+    "revision": "e0c3e5f9b944ad6f9a9f",
+    "url": "/react-org-chart/static/css/main.047fb8b0.chunk.css"
+  },
+  {
+    "revision": "5a0dd7edab95d7092223",
+    "url": "/react-org-chart/static/js/2.7317c897.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-org-chart/static/js/2.7317c897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c3e5f9b944ad6f9a9f",
+    "url": "/react-org-chart/static/js/main.f2b2a08d.chunk.js"
+  },
+  {
+    "revision": "e83535cddbc8f90a46ee906ffda9c3e4",
+    "url": "/react-org-chart/static/js/main.f2b2a08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf929761cfdd5e81a7f",
+    "url": "/react-org-chart/static/js/runtime-main.36560262.js"
+  },
+  {
+    "revision": "f8399445e0f391bfe9059f4b992b3657",
+    "url": "/react-org-chart/static/media/avatar-personnel.f8399445.svg"
+  }
+]);
